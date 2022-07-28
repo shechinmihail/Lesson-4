2a09ae7ada64ec31c5613777b19f07d976218b61
shechin <shechinmihail@mail.ru>
2022-07-28 22:02:02 +0500

    Начало положено!

---
+++ src/Lesson4/Main.java
@@ -0,0 +1,12 @@
+package Lesson4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("в одной тонне ? киллограмм, ? грамм и ? миллиграмм.");
+
+
+    }
+
+}
+
